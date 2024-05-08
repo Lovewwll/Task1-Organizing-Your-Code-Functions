99f03a3010d30ffe313ae303607d5c340a915745
Lovewwll <r2211745t@students.msu.ac.zw>
2024-05-08 17:39:43 +0200

    Update task_one.py
    i am done

--- task_one.py
+++ task_one.py
@@ -1,18 +1,19 @@
-def plot_data(my_file):
-    '''
-    This function plots a scatterplot provided with a coordinate text file path 
+import matplotlib.pyplot as plt
+import numpy as np
 
-    Parameters
-    ----------
-    my_file : STRING 
-        Required path to the file with coordinates in text format 
+def plot_data(file_obj):
+	# Read data from the file object
+	data = np.genfromtxt(file_obj, delimiter=' ')
 
-    Returns
-    ---------
-    Scatterplot 
-    '''
-    scatter_plot = None 
-    """
-    ==> Write your code here <==
-    """
-    return scatter_plot 
+	# Split data into x and y values
+	x_values = data[:, 0]
+	y_values = data[:, 1]
+
+	# Create the scatter plot
+	plt.scatter(x_values, y_values)
+	plt.xlabel('X Axis')
+	plt.ylabel('Y Axis')
+	plt.title('Scatter Plot of Data')
+	plt.show()
+with open('x_y_coordinates.txt', 'r') as file_obj:
+     plot_data(file_obj)
